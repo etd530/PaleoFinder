ac95e6662fc7a6cdf4252a1acc726f4bbef0d54a
etd530 <etd530@gmail.com>
2023-10-24 09:59:59 +0200

    Added printouts for debugging and fixed calling nodes.dmp where there should be merged.dmp

--- pseudogene_finder.py
+++ pseudogene_finder.py
@@ -578,14 +578,18 @@ def is_child(query_taxid, parent_taxid, taxdb_nodes, taxdb_merged):
 	taxdb.rename(columns={0: 'node', 1: 'parent', 2: 'rank'}, inplace = True)
 	while query_taxid != parent_taxid:
 		try:
+			print(query_taxid)
+			print(type(query_taxid))
+			print(taxdb['parent'][taxdb['node'] == query_taxid])
 			query_taxid = taxdb['parent'][taxdb['node'] == query_taxid].iloc[0]
 		except IndexError:
-			is_merged = os.system('grep -q ^' + str(query_taxid) + ', ' + taxdb_nodes)
+			print('WARNING: taxid %s not found in nodes.tmp. Checking if it has been merged into another taxid.' % str(query_taxid))
+			is_merged = os.system('grep -q ^' + str(query_taxid) + ', ' + taxdb_merged)
 			if is_merged:
 				merged_nodes = subprocess.run(['grep', '^' + str(query_taxid) + ',', taxdb_merged], stdout=subprocess.PIPE).stdout.decode('utf-8')
 				query_taxid = merged_nodes.strip().split(',')[1]
 			else:
-				print('WARNING: taxid %s not found in nodes.dmp and not merged into another (not found in merged.dmp). Will treat as not belonging to the specified parent taxid.' % str(query_taxid))
+				print('WARNING: taxid %s not merged into another (not found in merged.dmp). Will treat as not belonging to the specified parent taxid.' % str(query_taxid))
 				return(False)
 		if query_taxid == 1:
 			return(False)
@@ -609,17 +613,19 @@ def find_lca(taxid1, taxid2, taxdb_nodes, taxdb_merged):
 	path1 = [taxid1]
 	path2 = [taxid2]
 	while taxid1 not in path2 and taxid2 not in path1:
-		print(taxid1)
-		print(taxid2)
 		try:
+			print(taxid1)
+			print(type(taxid1))
+			print(taxdb['parent'][taxdb['node'] == taxid1])
 			taxid1 = taxdb['parent'][taxdb['node'] == taxid1].iloc[0]
 		except IndexError:
+			print('WARNING: taxid %s not found in nodes.dmp. Checking if it has been merged into another taxid.' % str(taxid1))
 			is_merged = os.system('grep -q ^' + str(taxid1) + ', ' + taxdb_merged)
 			if is_merged:
 				merged_nodes = subprocess.run(['grep', '^' + str(taxid1) + ',', taxdb_merged], stdout = subprocess.PIPE).stdout.decode('utf-8')
 				taxid1 = merged_nodes.strip().split(',')[1]
 			else:
-				print('WARNING: taxid %s not found in nodes.dmp and not merged into another (not found in merged.dmp). Will take the other input taxid as the LCA of itself.')
+				print('WARNING: taxid %s not merged into another (not found in merged.dmp). Will take the other input taxid as the LCA of itself.' % str(taxid1))
 				return(path2[0])
 		else:
 			path1.append(taxid1)
@@ -629,12 +635,13 @@ def find_lca(taxid1, taxid2, taxdb_nodes, taxdb_merged):
 			print(taxdb['parent'][taxdb['node'] == taxid2])
 			taxid2 = taxdb['parent'][taxdb['node'] == taxid2].iloc[0]
 		except IndexError:
+			print('WARNING: taxid %s not found in nodes.dmp. Checking if it has been merged into another taxid.' % str(taxid2))
 			is_merged = os.system('grep -q ^' + str(taxid2) + ', ' + taxdb_merged)
 			if is_merged:
 				merged_nodes = subprocess.run(['grep', str(taxid2) + ',', taxdb_merged], stdout = subprocess.PIPE).stdout.decode('utf-8')
 				taxid2 = merged_nodes.strip().split(',')[1]
 			else:
-				print('WARNING: taxid %s not found in nodes.dmp and not merged into another (not found in merged.dmp). Will take the other input taxid as the LCA of itself.')
+				print('WARNING: taxid %s not merged into another (not found in merged.dmp). Will take the other input taxid as the LCA of itself.' % str(taxid2))
 				return(path1[0])
 		else:
 			path2.append(taxid2)
