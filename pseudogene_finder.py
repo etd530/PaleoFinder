b97992cac6cc78a3fc166b4be643fd34fd3ec65f
etd530 <etd530@gmail.com>
2024-07-25 10:10:20 +0200

    Updated subset_gff() to work with the new naming scheme

--- pseudogene_finder.py
+++ pseudogene_finder.py
@@ -1254,7 +1254,7 @@ def subset_gff(blastp_filtered_summary):
 				else:
 					gff_entry = line.strip().split('\t')
 					peptide_number = gff_entry[8].split(';')[0].replace('ID=pseudogene_', '')
-					peptide_name = gff_entry[0] + file.replace('extended_peptides_all_gff/', '').replace('pseudogene_finder', '').replace('reconstructed_peptides.gff', '') + 'pseudopeptide_candidate_' + peptide_number
+					peptide_name = gff_entry[0] + '___' + file.replace('extended_peptides_all_gff/', '').replace('pseudogene_finder', '').replace('reconstructed_peptides.gff', '') + '___' + 'pseudopeptide_candidate_' + peptide_number
 					# print(peptide_name)
 					if peptide_name in seqids2keep:
 						file_string = file_string + line
